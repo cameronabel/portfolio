b52160605e6908e3b67d60e9512452fe7a627894
Cameron Abel <cameronabel@gmail.com>
2022-10-31 14:37:09 -0700

    added dark mode and tweaked txt-overlay

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+window.onload = function() {
+  
+
+  let darkMode = document.getElementById('darkmode');
+  if (localStorage.getItem('darkMode') === 'true') {
+    darkMode.checked = true;
+    enableDarkMode()
+  }
+  darkMode.addEventListener("change", function() {
+
+    if (darkMode.checked){
+      localStorage.setItem('darkMode', true);
+    } else {
+      localStorage.setItem('darkMode', false);
+    }
+
+    if (localStorage.getItem('darkMode') === 'true') {
+      enableDarkMode();
+    } else {
+      disableDarkMode();
+    }
+  });
+
+
+  };
+
+function enableDarkMode() {
+  document.body.style.backgroundColor = "#121212";
+  const shadedAreas = document.querySelectorAll('.shaded');
+  shadedAreas.forEach(element => {
+    element.classList.remove('shaded');
+    element.classList.add('shaded-dm');
+  })
+  const bodyAreas = document.querySelectorAll('.body-container');
+  bodyAreas.forEach(element => {
+    element.style.color = '#DDDDDD';
+  })
+  const genericBanners = document.querySelectorAll('.generic-banner')
+  genericBanners.forEach(element => {
+    element.classList.remove('generic-banner');
+    element.classList.add('generic-banner-dm');
+  })
+  document.getElementById('dm-switch-area').classList.add('icon-dm')
+}
+
+function disableDarkMode() {
+  document.body.style.backgroundColor = "white";
+  const shadedAreas = document.querySelectorAll('.shaded-dm');
+  shadedAreas.forEach(element => {
+    element.classList.remove('shaded-dm');
+    element.classList.add('shaded');
+  })
+  const bodyAreas = document.querySelectorAll('.body-container');
+  bodyAreas.forEach(element => {
+    element.style.color = 'black';
+  })
+  document.getElementById('dm-switch-area').classList.remove('icon-dm');
+}
